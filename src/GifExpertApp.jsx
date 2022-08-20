67508a636a81bcd87ce2238259e858cbc17b84f1
Edmundo Pichardo <ce.pichardo@gmail.com>
2022-08-20 19:49:44 -0400

    Create GifExpertApp Component and inserted styles.css

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,6 +1,14 @@
 export const GifExpertApp = () => {
-   const  apiKey = 'eLExLPtDE06VxhwSZ4ZpRpaBss0IWc48';
-  return (
-    <h1>GifExpertApp</h1>
-  )
+   const apiKey = 'eLExLPtDE06VxhwSZ4ZpRpaBss0IWc48';
+   return (
+      <>
+         {/* Title */}
+         <h1>GifExpertApp</h1>
+         
+         {/* Input */}
+         
+         {/* Listado de Gif */}
+            {/* Gif Item */}
+      </>
+   )
 }
