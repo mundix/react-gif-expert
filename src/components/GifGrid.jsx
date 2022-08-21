631cd180f34b7c1fbe2e911c9a03ec39dd8c0757
Edmundo Pichardo <ce.pichardo@gmail.com>
2022-08-21 07:27:45 -0400

    Called one time getGifs function on show component

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,4 +1,5 @@
 
+import { useEffect } from 'react';
 import { getGifs } from '../helpers/getGifs';
 
 export const GifGrid = ({ category }) => {
@@ -6,12 +7,13 @@ export const GifGrid = ({ category }) => {
 
    // No debe llamarse directamente las funcinoes por que siempre que se renderiza lo hace varias
    // veces, no improta si actualiza vuelve y llama el api , para eso vamos a usar useEffect();
-   getGifs(category);
+   useEffect(() => {
+      getGifs(category);
+   }, [])
 
    return (
       <>
          <h3>{category}</h3>
-         <p>Hola Mundo</p>
       </>
    )
 }
--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 export const getGifs = async (category) => {
 
-   const url = `https://api.giphy.com/v1/gifs/search?api_key=eLExLPtDE06VxhwSZ4ZpRpaBss0IWc48&q=${category}&limit=20`;
+   const url = `https://api.giphy.com/v1/gifs/search?api_key=eLExLPtDE06VxhwSZ4ZpRpaBss0IWc48&q=${category}&limit=10`;
    const resp = await fetch(url);
    const { data } = await resp.json();
    // console.log(resp);
@@ -10,5 +10,6 @@ export const getGifs = async (category) => {
       title: img.title,
       url: img.images.downsized_medium.url
    }));
+   console.log(gifs);
    return gifs;
 }
