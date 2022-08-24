85d8818b1dfac5aae4d356a380fdeae6afe2ed99
Edmundo Pichardo <ce.pichardo@gmail.com>
2022-08-24 06:32:14 -0400

    Evalundo estado inicial de GifGrid

--- test/components/GifGrid.test.jsx
+++ test/components/GifGrid.test.jsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { GifGrid } from '../../src/components/GifGrid';
 
 describe('Pruebas en <GifGrid/>', () => { 
@@ -8,6 +8,11 @@ describe('Pruebas en <GifGrid/>', () => {
       const category = 'One Punch';
 
       render(<GifGrid category={category}/>);
+      //Evaluando el estado incial
+      expect(screen.getByText('Loading ...'));
+      expect(screen.getByText(category));
+      // render().debug();
+
 
     })
 
