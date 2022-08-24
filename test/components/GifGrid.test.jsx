6ba536862f9331c388af0c7c1c651853e26f02da
Edmundo Pichardo <ce.pichardo@gmail.com>
2022-08-24 06:44:33 -0400

    Probando render de imagenes usando useFetchGifs

--- test/components/GifGrid.test.jsx
+++ test/components/GifGrid.test.jsx
@@ -1,20 +1,58 @@
 import { render, screen } from '@testing-library/react';
 import { GifGrid } from '../../src/components/GifGrid';
+import { useFetchGifs } from '../../src/hooks/useFetchGifs';
 
-describe('Pruebas en <GifGrid/>', () => { 
+jest.mock('../../src/hooks/useFetchGifs');
 
-   test('Debe de mostrar el loading incialmente', () => { 
+describe('Pruebas en <GifGrid/>', () => {
+
+   test('Debe de mostrar el loading incialmente', () => {
 
       const category = 'One Punch';
 
-      render(<GifGrid category={category}/>);
+      //se usa mockReturnValue para enviar a al hook useFetchGifs 
+      useFetchGifs.mockReturnValue({
+         images: [],
+         isLoading: true
+      });
+
+      render(<GifGrid category={category} />);
       //Evaluando el estado incial
       expect(screen.getByText('Loading ...'));
       expect(screen.getByText(category));
       // render().debug();
 
 
-    })
+   })
+
+   test('Debe de mostrarlas imagenes de fetchGifs', () => {
+
+      //debo hacer el mock de hook , par apoder renderizar l componetne
+      // useFetchGifs
+      const category = 'Saitama';
+
+      const gifs = [
+         {
+            id: 'ABC',
+            title: 'Saitama',
+            url: 'https://localhost/saitama.jpg'
+         },
+         {
+            id: '123',
+            title: 'Goku',
+            url: 'https://localhost/goku.jpg'
+         }
+      ];
+      useFetchGifs.mockReturnValue({
+         images: gifs,
+         isLoading: false
+      });
+
+      render(<GifGrid category={category}/>);
+      expect(screen.getAllByRole('img').length).toBe(2);
+      // render().debug();
+
+   });
 
 
- })
+})
