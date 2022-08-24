efcc7a5c56e87210c8c4c19bd90555d57f9b7178
Edmundo Pichardo <ce.pichardo@gmail.com>
2022-08-24 06:24:26 -0400

    Probando onSubmit en AddCategory si o no tiene valor el input

--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -29,7 +29,8 @@ export const AddCategory = ({onNewCategory}) => {
 
    return (
       // <form onSubmit={(event) => onSubmit(event)}>
-      <form onSubmit={onSubmit}>
+      // Se agrego el aria-label para que getByRole encuentr por form 
+      <form onSubmit={onSubmit} aria-label="form">
          <input
             type="text"
             placeholder="Buscar Gifs"
--- test/components/AddCategory.test.jsx
+++ test/components/AddCategory.test.jsx
@@ -18,4 +18,47 @@ describe('Probando componente <AddCategory/>', () => {
 
    });
 
+   test('Debe llamar onNewCategory si el input tiene un Valor', () => {
+
+      const inputValue = 'Saitama';
+      //TODO: ???
+      const onNewCategory = jest.fn();
+
+      // render(<AddCategory onNewCategory={() => {}}/>);
+      render(<AddCategory onNewCategory={onNewCategory} />);
+
+      const input = screen.getByRole('textbox');
+      const form = screen.getByRole('form');
+
+      fireEvent.input(input, { target: { value: inputValue } });
+
+      fireEvent.submit(form); //el form le manda la configuracion necesaria 
+      // Despes dehacer submit el campo de texto debe quedar vcio
+      expect(input.value).toBe('');
+      expect(onNewCategory).toHaveBeenCalled();
+      expect(onNewCategory).toHaveBeenCalledTimes(1);
+      expect(onNewCategory).not.toHaveBeenCalledTimes(2);
+      expect(onNewCategory).toHaveBeenCalledWith(inputValue); //ahi llama con el valor de la caja de text
+
+   });
+
+   test('No debe llamar el onNewCategory si el input esta vacio', () => {
+
+      const inputValue = '';
+      //TODO: ???
+      const onNewCategory = jest.fn();
+
+      // render(<AddCategory onNewCategory={() => {}}/>);
+      render(<AddCategory onNewCategory={onNewCategory} />);
+
+      const form = screen.getByRole('form');// Esto es por que se le agrego el aria-label='form'
+
+      fireEvent.submit(form); //el form le manda la configuracion necesaria 
+      // Despes dehacer submit el campo de texto debe quedar vcio
+      expect(onNewCategory).not.toHaveBeenCalled();
+      expect(onNewCategory).toHaveBeenCalledTimes(0);
+      expect(onNewCategory).not.toHaveBeenCalledWith(inputValue); //ahi llama con el valor de la caja de text
+
+   })
+
 })
